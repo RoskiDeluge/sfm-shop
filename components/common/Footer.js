d2f1eeadfd755be371423af39bc1ff627f0dbc0f
Roberto Delgado <roberto.delgado@gmail.com>
2020-07-31 22:41:11 -1000

    Replaced some links in footer

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => (
-
   <footer className="pt-5">
     <div className="custom-container mb-5 pb-5 pt-5">
       <div className="row">
         <div className="col-12 col-sm-6 col-md-4">
-          <p className="font-family-secondary font-size-display1 mb-4">
-            Commerce.js
-          </p>
+          <p className="font-family-secondary font-size-display1 mb-4">Commerce.js</p>
           <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
             <div className="pr-5">
               <a
@@ -45,20 +42,18 @@ const Footer = () => (
           </div>
         </div>
         <div className="col-12 col-sm-6 col-md-4">
-          <p className="font-family-secondary font-size-display1 mb-4">
-            Follow us
-          </p>
+          <p className="font-family-secondary font-size-display1 mb-4">Follow us</p>
           <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
             <div className="pr-5">
               <a
-                href="https://twitter.com/commercejs"
+                href="https://twitter.com/RoskiDeluge"
                 className="mb-3 d-block font-color-medium"
                 target="_blank"
               >
                 Twitter
               </a>
               <a
-                href="https://www.instagram.com/commerce.js/"
+                href="https://www.instagram.com/southfirstmedia/"
                 className="d-block font-color-medium"
                 target="_blank"
               >
@@ -84,16 +79,14 @@ const Footer = () => (
           </div>
         </div>
         <div className="col-12 col-md-4">
-          <p className="font-family-secondary font-size-display1 mb-3">
-            Newsletter
-          </p>
+          <p className="font-family-secondary font-size-display1 mb-3">Newsletter</p>
           <div className="position-relative">
             <input
               className="borderbottom border-color-gray400 h-48 w-100 px-3"
               placeholder="email address"
             />
             <button className="bg-transparent position-absolute right-0 top-50 translateY--50 pr-2 h-48">
-              <img src="/icon/arrow-long-right.svg" className="w-24" alt="Arrow icon"/>
+              <img src="/icon/arrow-long-right.svg" className="w-24" alt="Arrow icon" />
             </button>
           </div>
         </div>
@@ -128,7 +121,7 @@ const Footer = () => (
             </a>
           </div>
           <div className="font-color-brand font-size-caption py-4 text-right">
-          <a
+            <a
               href="https://commercejs.com/"
               className="font-color-brand font-size-caption text-uppercase text-center"
               target="_blank"
